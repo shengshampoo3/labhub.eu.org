491c4544152d5e2ce5bc801db5331360e57c766c
Alex <alex@alex-dev.me>
2024-04-15 11:20:50 -0600

    Disable video previews due to package's issue

--- src/components/previews/VideoPreview.tsx
+++ src/components/previews/VideoPreview.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from 'next-i18next'
 
 import axios from 'axios'
 import toast from 'react-hot-toast'
-import Plyr from 'plyr-react'
+// import Plyr from 'plyr-react'
 import { useAsync } from 'react-async-hook'
 import { useClipboard } from 'use-clipboard-copy'
 
@@ -22,57 +22,57 @@ import CustomEmbedLinkMenu from '../CustomEmbedLinkMenu'
 
 import 'plyr-react/plyr.css'
 
-const VideoPlayer: FC<{
-  videoName: string
-  videoUrl: string
-  width?: number
-  height?: number
-  thumbnail: string
-  subtitle: string
-  isFlv: boolean
-  mpegts: any
-}> = ({ videoName, videoUrl, width, height, thumbnail, subtitle, isFlv, mpegts }) => {
-  useEffect(() => {
-    // Really really hacky way to inject subtitles as file blobs into the video element
-    axios
-      .get(subtitle, { responseType: 'blob' })
-      .then(resp => {
-        const track = document.querySelector('track')
-        track?.setAttribute('src', URL.createObjectURL(resp.data))
-      })
-      .catch(() => {
-        console.log('Could not load subtitle.')
-      })
+// const VideoPlayer: FC<{
+//   videoName: string
+//   videoUrl: string
+//   width?: number
+//   height?: number
+//   thumbnail: string
+//   subtitle: string
+//   isFlv: boolean
+//   mpegts: any
+// }> = ({ videoName, videoUrl, width, height, thumbnail, subtitle, isFlv, mpegts }) => {
+//   useEffect(() => {
+//     // Really really hacky way to inject subtitles as file blobs into the video element
+//     axios
+//       .get(subtitle, { responseType: 'blob' })
+//       .then(resp => {
+//         const track = document.querySelector('track')
+//         track?.setAttribute('src', URL.createObjectURL(resp.data))
+//       })
+//       .catch(() => {
+//         console.log('Could not load subtitle.')
+//       })
 
-    if (isFlv) {
-      const loadFlv = () => {
-        // Really hacky way to get the exposed video element from Plyr
-        const video = document.getElementById('plyr')
-        const flv = mpegts.createPlayer({ url: videoUrl, type: 'flv' })
-        flv.attachMediaElement(video)
-        flv.load()
-      }
-      loadFlv()
-    }
-  }, [videoUrl, isFlv, mpegts, subtitle])
+//     if (isFlv) {
+//       const loadFlv = () => {
+//         // Really hacky way to get the exposed video element from Plyr
+//         const video = document.getElementById('plyr')
+//         const flv = mpegts.createPlayer({ url: videoUrl, type: 'flv' })
+//         flv.attachMediaElement(video)
+//         flv.load()
+//       }
+//       loadFlv()
+//     }
+//   }, [videoUrl, isFlv, mpegts, subtitle])
 
-  // Common plyr configs, including the video source and plyr options
-  const plyrSource = {
-    type: 'video',
-    title: videoName,
-    poster: thumbnail,
-    tracks: [{ kind: 'captions', label: videoName, src: '', default: true }],
-  }
-  const plyrOptions: Plyr.Options = {
-    ratio: `${width ?? 16}:${height ?? 9}`,
-    fullscreen: { iosNative: true },
-  }
-  if (!isFlv) {
-    // If the video is not in flv format, we can use the native plyr and add sources directly with the video URL
-    plyrSource['sources'] = [{ src: videoUrl }]
-  }
-  return <Plyr id="plyr" source={plyrSource as Plyr.SourceInfo} options={plyrOptions} />
-}
+//   // Common plyr configs, including the video source and plyr options
+//   const plyrSource = {
+//     type: 'video',
+//     title: videoName,
+//     poster: thumbnail,
+//     tracks: [{ kind: 'captions', label: videoName, src: '', default: true }],
+//   }
+//   const plyrOptions: Plyr.Options = {
+//     ratio: `${width ?? 16}:${height ?? 9}`,
+//     fullscreen: { iosNative: true },
+//   }
+//   if (!isFlv) {
+//     // If the video is not in flv format, we can use the native plyr and add sources directly with the video URL
+//     plyrSource['sources'] = [{ src: videoUrl }]
+//   }
+//   return <Plyr id="plyr" source={plyrSource as Plyr.SourceInfo} options={plyrOptions} />
+// }
 
 const VideoPreview: FC<{ file: OdFileObject }> = ({ file }) => {
   const { asPath } = useRouter()
@@ -112,16 +112,23 @@ const VideoPreview: FC<{ file: OdFileObject }> = ({ file }) => {
         ) : loading && isFlv ? (
           <Loading loadingText={t('Loading FLV extension...')} />
         ) : (
-          <VideoPlayer
-            videoName={file.name}
-            videoUrl={videoUrl}
-            width={file.video?.width}
-            height={file.video?.height}
-            thumbnail={thumbnail}
-            subtitle={subtitle}
-            isFlv={isFlv}
-            mpegts={mpegts}
-          />
+          <div>
+            <h1>Filename: {file.name}</h1>
+            <h3>Video URL: {videoUrl}</h3>
+            <h3>Thumbnail: {thumbnail}</h3>
+            <h3>Subtitle: {subtitle}</h3>
+            <h3>Is FLV: {isFlv}</h3>
+          </div>
+          // <VideoPlayer
+          //   videoName={file.name}
+          //   videoUrl={videoUrl}
+          //   width={file.video?.width}
+          //   height={file.video?.height}
+          //   thumbnail={thumbnail}
+          //   subtitle={subtitle}
+          //   isFlv={isFlv}
+          //   mpegts={mpegts}
+          // />
         )}
       </PreviewContainer>
 
