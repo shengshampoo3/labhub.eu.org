ff8246ef94c071182324b0107377c7200b08b8fb
Alex <alex@sudoalex.dev>
2024-09-11 23:16:50 -0600

    chore: Update footer in site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -58,7 +58,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    '<a href="https://t.me/NetLabHub" target="_blank" rel="noopener noreferrer">LabHub</a> - 2023',
+    `<span><a href="https://t.me/NetLabHub" target="_blank" rel="noopener noreferrer">LabHub</a> &copy 2021 - ${new Date().getFullYear()}</span>`,
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
