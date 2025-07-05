a2e49d041ba6df84a4a1c292529f2a8664776b01
shengshampoo3 <112081979+shengshampoo3@users.noreply.github.com>
2025-07-05 11:26:30 +0000

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -37,7 +37,7 @@ module.exports = {
   siteType: "website",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/public/',
+  baseDirectory: '/Pkg_sexn/',
 
   // Allow downloading folders
   allowFolderDownload: false,
